62faa2249645e0b7cc6f7b99be9e35365d36e309
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2024-02-20 14:22:27 +0000

    use FileIO

--- src/test/scala/PekkoSpec.scala
+++ src/test/scala/PekkoSpec.scala
@@ -1,6 +1,7 @@
 import org.apache.pekko.Done
 import org.apache.pekko.actor.typed.ActorSystem
 import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import org.apache.pekko.stream.IOResult
 import org.apache.pekko.stream.connectors.csv.scaladsl.{CsvParsing, CsvToMap}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
@@ -9,6 +10,8 @@ import scala.concurrent.Future
 import org.apache.pekko.stream.scaladsl.{Sink, Source}
 import org.apache.pekko.util.ByteString
 import uk.gov.nationalarchives.tdr.validation.{FileRow, Metadata}
+
+import java.nio.file.Paths
 
 class PekkoSpec extends AnyFlatSpec {
 
@@ -29,20 +32,23 @@ class PekkoSpec extends AnyFlatSpec {
       |rnd_875.jpg,Open,,,,,No,,,No,,English,,,,
       |rnd_789.jpg,Closed,,,,,No,,,No,,English,,,,ClosurePeriod: EMPTY_VALUE_ERROR | FoiExemptionCode: EMPTY_VALUE_ERROR | FoiExemptionAsserted: EMPTY_VALUE_ERROR
       |rnd_297.jpg,Open,,,,,No,,,No,,English,,,,""".stripMargin // also should complete once notices end of array
-
+  import org.apache.pekko.stream.scaladsl._
+  val file = Paths.get("/home/ian/Downloads/TDR-2024-HNB.csv")
  private val future =
-    Source
-      .single(ByteString(input))
+    FileIO.fromPath(file)
       .via(CsvParsing.lineScanner())
       .via(CsvToMap.toMapAsStrings())
       .map(mapToLineRow)
-      .runWith(Sink.fold(List[FileRow]())(_ :+ _))
+      .runWith(Sink.foreach(println))
 
   future.onComplete { a =>
     println(a)
     actorSystem.terminate()
   }
 
+
+
+  val foreach: Future[IOResult] = FileIO.fromPath(file).to(Sink.ignore).run()
   private def mapToLineRow (input: Map[String, String]) = {
      FileRow(input("Filename"),input.keys.map{ a => Metadata(a,input(a)) }.toList)
   }
