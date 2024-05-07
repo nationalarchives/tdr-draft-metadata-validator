18311a193488a5d83f9d16b1ebb77c03eb7938ad
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2024-05-07 16:08:10 +0100

    merged all scala stward updates

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,20 +2,20 @@ import sbt._
 
 object Dependencies {
 
-  private val log4CatsVersion = "2.6.0"
-  private val mockitoScalaVersion = "1.17.27"
+  private val log4CatsVersion = "2.7.0"
+  private val mockitoScalaVersion = "1.17.31"
 
   lazy val scalaCsv = "com.github.tototoshi" %% "scala-csv" % "1.3.10"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
   lazy val metadataValidation = "uk.gov.nationalarchives" %% "tdr-metadata-validation" % "0.0.23"
-  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.372"
-  lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.144"
-  lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.187"
+  lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.375"
+  lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.157"
+  lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.197"
   lazy val typeSafeConfig = "com.typesafe" % "config" % "1.4.3"
   lazy val awsLambda = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
-  lazy val awsLambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.11.4"
-  lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.23.17"
-  lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.105"
+  lazy val awsLambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.11.5"
+  lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.23.21"
+  lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.169"
   lazy val log4catsSlf4j = "org.typelevel" %% "log4cats-slf4j" % log4CatsVersion
   lazy val mockitoScala = "org.mockito" %% "mockito-scala" % mockitoScalaVersion
   lazy val mockitoScalaTest = "org.mockito" %% "mockito-scala-scalatest" % mockitoScalaVersion
