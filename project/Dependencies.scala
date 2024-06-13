e5e3bbe1b46bbc52423947df2f21b5ecb230e7bb
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2024-06-13 08:30:41 +0000

    Update s3-utils to 0.1.185 in scala-steward-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   lazy val awsLambda = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
   lazy val awsLambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.11.5"
   lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.25.69"
-  lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.181"
+  lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.185"
   lazy val log4catsSlf4j = "org.typelevel" %% "log4cats-slf4j" % log4CatsVersion
   lazy val mockitoScala = "org.mockito" %% "mockito-scala" % mockitoScalaVersion
   lazy val mockitoScalaTest = "org.mockito" %% "mockito-scala-scalatest" % mockitoScalaVersion
