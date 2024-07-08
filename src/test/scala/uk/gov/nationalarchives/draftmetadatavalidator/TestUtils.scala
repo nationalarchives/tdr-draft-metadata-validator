cd84b2cb2fe673c40754fda6551a4160894cadb6
Thanh <thanh.lam@nationalarchives.gov.uk>
2024-07-08 16:50:19 +0100

    scalafmt

--- src/test/scala/uk/gov/nationalarchives/draftmetadatavalidator/TestUtils.scala
+++ src/test/scala/uk/gov/nationalarchives/draftmetadatavalidator/TestUtils.scala
@@ -7,19 +7,20 @@ import graphql.codegen.types.PropertyType.Defined
 
 object TestUtils {
 
-  def createCustomMetadata(name: String, fullName: String, exportOrdinal: Int, dataType: DataType = Text, allowExport: Boolean = true, editable: Boolean = true): CustomMetadata = CustomMetadata(
-    name,
-    None,
-    Some(fullName),
-    Defined,
-    Some("MandatoryClosure"),
-    dataType,
-    editable,
-    multiValue = false,
-    Some("Open"),
-    1,
-    Nil,
-    Option(exportOrdinal),
-    allowExport = allowExport
-  )
+  def createCustomMetadata(name: String, fullName: String, exportOrdinal: Int, dataType: DataType = Text, allowExport: Boolean = true, editable: Boolean = true): CustomMetadata =
+    CustomMetadata(
+      name,
+      None,
+      Some(fullName),
+      Defined,
+      Some("MandatoryClosure"),
+      dataType,
+      editable,
+      multiValue = false,
+      Some("Open"),
+      1,
+      Nil,
+      Option(exportOrdinal),
+      allowExport = allowExport
+    )
 }
