b369b6fe2cac72b3a48fea08f76b2f9af8799f07
Thanh <thanh.lam@nationalarchives.gov.uk>
2024-09-24 11:43:17 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,6 @@ object Dependencies {
   lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.387"
   lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.181"
   lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.213"
-
   lazy val typeSafeConfig = "com.typesafe" % "config" % "1.4.3"
   lazy val awsLambda = "com.amazonaws" % "aws-lambda-java-core" % "1.2.3"
   lazy val awsLambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.14.0"
