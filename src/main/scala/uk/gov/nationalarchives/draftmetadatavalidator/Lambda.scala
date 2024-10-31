178cf37527178b859b0f0177ba443ceeed44b1e0
TomJKing <tom.king@nationalarchives.gov.uk>
2024-10-31 13:52:13 +0000

    Use correct error key

--- src/main/scala/uk/gov/nationalarchives/draftmetadatavalidator/Lambda.scala
+++ src/main/scala/uk/gov/nationalarchives/draftmetadatavalidator/Lambda.scala
@@ -25,7 +25,7 @@ import uk.gov.nationalarchives.draftmetadatavalidator.Lambda.{ValidationExecutio
 import uk.gov.nationalarchives.draftmetadatavalidator.utils.MetadataUtils
 import uk.gov.nationalarchives.tdr.GraphQLClient
 import uk.gov.nationalarchives.tdr.keycloak.{KeycloakUtils, TdrKeycloakDeployment}
-import uk.gov.nationalarchives.tdr.schemautils.SchemaUtils.convertToAlternateKey
+import uk.gov.nationalarchives.tdr.schemautils.SchemaUtils.{convertToAlternateKey, convertToValidationKey}
 import uk.gov.nationalarchives.tdr.validation.FileRow
 import uk.gov.nationalarchives.tdr.validation.schema.JsonSchemaDefinition.{BASE_SCHEMA, CLOSURE_SCHEMA_CLOSED, CLOSURE_SCHEMA_OPEN, REQUIRED_SCHEMA}
 import uk.gov.nationalarchives.tdr.validation.schema.{JsonSchemaDefinition, MetadataValidationJsonSchema}
@@ -154,17 +154,19 @@ class Lambda extends RequestHandler[java.util.Map[String, Object], APIGatewayPro
   }
 
   private def validateDuplicateHeaders(validationParameters: ValidationParameters, csvHandler: CSVHandler): IO[Unit] = {
+    def duplicateError(header: String): Error = {
+      val errorKey = convertToValidationKey("tdrFileHeader", header)
+      val duplicateFileError = FileError.DUPLICATE_HEADER.toString
+      Error(duplicateFileError, header, "duplicate", s"$duplicateFileError.$errorKey.duplicate")
+    }
+
     val filePath = getFilePath(validationParameters)
     val headers = csvHandler.loadHeaders(filePath).getOrElse(Nil)
     if (headers.size > headers.toSet.size) {
-      val duplicateHeaders = headers.groupBy(identity).collect { case (x, ys) if ys.lengthCompare(1) > 0 => x }
-      val errors = duplicateHeaders
-        .map(h => {
-          val duplicateFileError = FileError.DUPLICATE_HEADER.toString
-          Error(duplicateFileError, h, "duplicate", s"$duplicateFileError.$h.duplicate")
-        })
-        .toSet
-      val validationErrors = ValidationErrors(validationParameters.consignmentId.toString, errors)
+      val duplicateHeaders = headers.groupBy(identity).collect {
+        case (identifier, values) if values.size > 1 => identifier
+      }
+      val validationErrors = ValidationErrors(validationParameters.consignmentId.toString, duplicateHeaders.map(duplicateError).toSet)
       IO.raiseError(ValidationExecutionError(ErrorFileData(validationParameters, FileError.DUPLICATE_HEADER, List(validationErrors)), List.empty[FileRow]))
     } else { IO.unit }
   }
