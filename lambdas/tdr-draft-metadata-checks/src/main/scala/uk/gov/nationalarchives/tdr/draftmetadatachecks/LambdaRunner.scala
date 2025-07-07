9ace8072ddeddc265000bc7c16003eda0408d53f
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-07-07 10:50:35 +0100

    don't use real ID

--- lambdas/tdr-draft-metadata-checks/src/main/scala/uk/gov/nationalarchives/tdr/draftmetadatachecks/LambdaRunner.scala
+++ lambdas/tdr-draft-metadata-checks/src/main/scala/uk/gov/nationalarchives/tdr/draftmetadatachecks/LambdaRunner.scala
@@ -3,6 +3,6 @@ package uk.gov.nationalarchives.tdr.draftmetadatachecks
 import scala.jdk.CollectionConverters.MapHasAsJava
 
 object LambdaRunner extends App {
-  val input = Map("consignmentId" -> "03487bf0-07f8-45e2-9ce8-2a5642b865ef".asInstanceOf[Object]).asJava
+  val input = Map("consignmentId" -> "03487bf0-07f8-45e2-9ce8-2a5642b86aaa".asInstanceOf[Object]).asJava
   println(new Lambda().handleRequest(input, null))
 }
