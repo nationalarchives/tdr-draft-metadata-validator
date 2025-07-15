c0e4c7b7b7d05ac9570a09a27da034c9a097085f
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-07-15 11:39:02 +0100

    tidy up

--- lambdas/tdr-draft-metadata-checks/src/test/scala/uk/gov/nationalarchives/tdr/draftmetadatachecks/LambdaSpec.scala
+++ lambdas/tdr-draft-metadata-checks/src/test/scala/uk/gov/nationalarchives/tdr/draftmetadatachecks/LambdaSpec.scala
@@ -7,18 +7,14 @@ import com.github.tomakehurst.wiremock.stubbing.{ServeEvent, StubMapping}
 import graphql.codegen.AddOrUpdateBulkFileMetadata.{addOrUpdateBulkFileMetadata => afm}
 import graphql.codegen.UpdateConsignmentMetadataSchemaLibraryVersion.{updateConsignmentMetadataSchemaLibraryVersion => ucslv}
 import graphql.codegen.UpdateConsignmentStatus.{updateConsignmentStatus => ucs}
-import graphql.codegen.types._
-import io.circe.generic.auto._
-import io.circe.parser.decode
 import org.mockito.MockitoSugar.mock
 import org.scalatest.matchers.must.Matchers.{be, convertToAnyMustWrapper}
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
-import uk.gov.nationalarchives.draftmetadata.FileTestData
 import uk.gov.nationalarchives.draftmetadata.TestUtils.{fileTestData, filesWithUniquesAssetIdKeyResponse}
 
 import java.nio.file.{Files, Paths}
 import java.text.SimpleDateFormat
-import java.util.{Date, UUID}
+import java.util.Date
 import scala.io.Source
 import scala.jdk.CollectionConverters.{CollectionHasAsScala, MapHasAsJava}
 
@@ -29,26 +25,13 @@ class LambdaSpec extends ExternalServicesSpec {
   val pattern = "yyyy-MM-dd"
   val dateFormat = new SimpleDateFormat(pattern)
 
-  private def checkFileError(errorFile: String) = {
-    mockS3ErrorFilePutResponse()
-    val input = Map("consignmentId" -> consignmentId).asJava
-    new Lambda().handleRequest(input, mockContext)
-
-    val s3Interactions: Iterable[ServeEvent] = wiremockS3.getAllServeEvents.asScala.filter(serveEvent => serveEvent.getRequest.getMethod == RequestMethod.PUT).toList
-    s3Interactions.size shouldBe 1
-
-    val errorWriteRequest = s3Interactions.head
-    val rawErrorFileData = errorWriteRequest.getRequest.getBodyAsString
-
-    // Extract just the JSON part from between the first { and last }
-    val errorFileData = rawErrorFileData.substring(
-      rawErrorFileData.indexOf("{"),
-      rawErrorFileData.lastIndexOf("}") + 1
+  def mockS3GetResponse(fileName: String): StubMapping = {
+    val filePath = getClass.getResource(s"/$fileName").getFile
+    val bytes = Files.readAllBytes(Paths.get(filePath))
+    wiremockS3.stubFor(
+      get(urlEqualTo(s"/$consignmentId/sample.csv"))
+        .willReturn(aResponse().withStatus(200).withBody(bytes))
     )
-
-    val today = dateFormat.format(new Date)
-    val expectedErrorData: String = Source.fromResource(errorFile).getLines.mkString(System.lineSeparator()).replace("$today", today)
-    errorFileData should be(expectedErrorData)
   }
 
   "handleRequest" should "download the draft metadata csv file, validate, save empty error file to s3 if it has no errors" in {
@@ -217,13 +200,26 @@ class LambdaSpec extends ExternalServicesSpec {
     response.get("error") must be("")
   }
 
-  def mockS3GetResponse(fileName: String): StubMapping = {
-    val filePath = getClass.getResource(s"/$fileName").getFile
-    val bytes = Files.readAllBytes(Paths.get(filePath))
-    wiremockS3.stubFor(
-      get(urlEqualTo(s"/$consignmentId/sample.csv"))
-        .willReturn(aResponse().withStatus(200).withBody(bytes))
+  private def checkFileError(errorFile: String) = {
+    mockS3ErrorFilePutResponse()
+    val input = Map("consignmentId" -> consignmentId).asJava
+    new Lambda().handleRequest(input, mockContext)
+
+    val s3Interactions: Iterable[ServeEvent] = wiremockS3.getAllServeEvents.asScala.filter(serveEvent => serveEvent.getRequest.getMethod == RequestMethod.PUT).toList
+    s3Interactions.size shouldBe 1
+
+    val errorWriteRequest = s3Interactions.head
+    val rawErrorFileData = errorWriteRequest.getRequest.getBodyAsString
+
+    // Extract just the JSON part from between the first { and last }
+    val errorFileData = rawErrorFileData.substring(
+      rawErrorFileData.indexOf("{"),
+      rawErrorFileData.lastIndexOf("}") + 1
     )
+
+    val today = dateFormat.format(new Date)
+    val expectedErrorData: String = Source.fromResource(errorFile).getLines.mkString(System.lineSeparator()).replace("$today", today)
+    errorFileData should be(expectedErrorData)
   }
 
   def mockS3ErrorFilePutResponse(): StubMapping = {
@@ -233,67 +229,6 @@ class LambdaSpec extends ExternalServicesSpec {
     )
   }
 
-  private def expectedFileMetadataInput(fileTestData: List[FileTestData]): List[AddOrUpdateFileMetadata] = {
-    List(
-      AddOrUpdateFileMetadata(
-        fileTestData.find(_.filePath == "test/test3.txt").get.fileId,
-        List(
-          AddOrUpdateMetadata("DescriptionClosed", "false"),
-          AddOrUpdateMetadata("FoiExemptionCode", ""),
-          AddOrUpdateMetadata("DescriptionAlternate", ""),
-          AddOrUpdateMetadata("former_reference_department", ""),
-          AddOrUpdateMetadata("ClosurePeriod", ""),
-          AddOrUpdateMetadata("TitleAlternate", ""),
-          AddOrUpdateMetadata("TitleClosed", "false"),
-          AddOrUpdateMetadata("file_name_translation", ""),
-          AddOrUpdateMetadata("ClosureType", "Open"),
-          AddOrUpdateMetadata("description", "eee"),
-          AddOrUpdateMetadata("FoiExemptionAsserted", ""),
-          AddOrUpdateMetadata("ClosureStartDate", ""),
-          AddOrUpdateMetadata("Language", "English"),
-          AddOrUpdateMetadata("end_date", "")
-        )
-      ),
-      AddOrUpdateFileMetadata(
-        fileTestData.find(_.filePath == "test/test1.txt").get.fileId,
-        List(
-          AddOrUpdateMetadata("DescriptionClosed", "false"),
-          AddOrUpdateMetadata("FoiExemptionCode", "27(1);27(2)"),
-          AddOrUpdateMetadata("DescriptionAlternate", ""),
-          AddOrUpdateMetadata("former_reference_department", ""),
-          AddOrUpdateMetadata("ClosurePeriod", "33;44"),
-          AddOrUpdateMetadata("TitleAlternate", "title"),
-          AddOrUpdateMetadata("TitleClosed", "true"),
-          AddOrUpdateMetadata("file_name_translation", ""),
-          AddOrUpdateMetadata("ClosureType", "Closed"),
-          AddOrUpdateMetadata("description", "hello"),
-          AddOrUpdateMetadata("FoiExemptionAsserted", "1990-01-01 00:00:00.0"),
-          AddOrUpdateMetadata("ClosureStartDate", "1990-01-01 00:00:00.0"),
-          AddOrUpdateMetadata("Language", "English"),
-          AddOrUpdateMetadata("end_date", "1990-01-01 00:00:00.0")
-        )
-      ),
-      AddOrUpdateFileMetadata(
-        fileTestData.find(_.filePath == "test/test2.txt").get.fileId,
-        List(
-          AddOrUpdateMetadata("DescriptionClosed", "false"),
-          AddOrUpdateMetadata("FoiExemptionCode", ""),
-          AddOrUpdateMetadata("DescriptionAlternate", ""),
-          AddOrUpdateMetadata("former_reference_department", ""),
-          AddOrUpdateMetadata("ClosurePeriod", ""),
-          AddOrUpdateMetadata("TitleAlternate", ""),
-          AddOrUpdateMetadata("TitleClosed", "false"),
-          AddOrUpdateMetadata("file_name_translation", ""),
-          AddOrUpdateMetadata("ClosureType", "Open"),
-          AddOrUpdateMetadata("description", "www"),
-          AddOrUpdateMetadata("FoiExemptionAsserted", ""),
-          AddOrUpdateMetadata("ClosureStartDate", ""),
-          AddOrUpdateMetadata("Language", "English"),
-          AddOrUpdateMetadata("end_date", "")
-        )
-      )
-    )
-  }
 }
 
 case class UpdateConsignmentStatusGraphqlRequestData(query: String, variables: ucs.Variables)
