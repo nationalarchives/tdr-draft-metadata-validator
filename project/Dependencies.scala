963f58d2a83be0a430ead14875955585401f2065
Thanh Lam <thanh.lam@nationalarchives.gov.uk>
2025-08-12 10:53:49 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,20 +6,20 @@ object Dependencies {
   private val mockitoScalaVersion = "2.0.0"
   private val circeVersion = "0.14.14"
 
-  val metadataSchemaVersion = "0.0.76"
+  val metadataSchemaVersion = "0.0.77"
 
   lazy val scalaCsv = "com.github.tototoshi" %% "scala-csv" % "2.0.0"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
-  lazy val metadataValidation = "uk.gov.nationalarchives" %% "tdr-metadata-validation" % "0.0.162" exclude ("uk.gov.nationalarchives", "da-metadata-schema")
+  lazy val metadataValidation = "uk.gov.nationalarchives" %% "tdr-metadata-validation" % "0.0.163" exclude ("uk.gov.nationalarchives", "da-metadata-schema")
   lazy val metadataSchema = "uk.gov.nationalarchives" %% "da-metadata-schema" % metadataSchemaVersion
   lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.426"
-  lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.245"
-  lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.252"
+  lazy val graphqlClient = "uk.gov.nationalarchives" %% "tdr-graphql-client" % "0.0.246"
+  lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.253"
   lazy val typeSafeConfig = "com.typesafe" % "config" % "1.4.4"
   lazy val awsLambda = "com.amazonaws" % "aws-lambda-java-core" % "1.3.0"
   lazy val awsLambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.16.1"
   lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.289"
-  lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.32.19"
+  lazy val awsSsm = "software.amazon.awssdk" % "ssm" % "2.32.20"
   lazy val log4catsSlf4j = "org.typelevel" %% "log4cats-slf4j" % log4CatsVersion
   lazy val slf4jSimple = "org.slf4j" % "slf4j-simple" % "2.0.17"
   lazy val mockitoScala = "org.mockito" %% "mockito-scala" % mockitoScalaVersion
